538c5ee1e35a28fcf7e2b804dc47bceaa6ba24e8
USER <USER@HTY-20240718AYX>
2025-03-28 14:57:45 +0800

    添加项目文件。

---
+++ NodeManager .cs
@@ -0,0 +1,133 @@
+﻿using Opc.Ua;
+using Opc.Ua.Server;
+using System;
+using System.Collections.Generic;
+
+namespace SampleOpcUaServer
+{
+    internal class NodeManager : CustomNodeManager2
+    {
+        public NodeManager(IServerInternal server, params string[] namespaceUris)
+            : base(server, namespaceUris)
+        {
+        }
+
+        public NodeManager(IServerInternal server, ApplicationConfiguration configuration, params string[] namespaceUris)
+            : base(server, configuration, namespaceUris)
+        {
+        }
+
+        protected override NodeStateCollection LoadPredefinedNodes(ISystemContext context)
+        {
+            FolderState root = CreateFolder(null, null, "Root");
+            root.AddReference(ReferenceTypes.Organizes, true, ObjectIds.ObjectsFolder); // 将节点添加到服务器根节点
+            root.EventNotifier = EventNotifiers.SubscribeToEvents;
+            AddRootNotifier(root);
+
+            CreateVariable(root, null, "Test", BuiltInType.Int64, ValueRanks.Scalar);
+
+            return new NodeStateCollection(new List<NodeState> { root });
+        }
+
+        protected virtual FolderState CreateFolder(NodeState? parent, string? path, string name)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                path = parent?.NodeId.Identifier is string id ? id + "_" + name : name;
+
+            FolderState folder = new FolderState(parent);
+            folder.SymbolicName = name;
+            folder.ReferenceTypeId = ReferenceTypes.Organizes;
+            folder.TypeDefinitionId = ObjectTypeIds.FolderType;
+            folder.NodeId = new NodeId(path, NamespaceIndex);
+            folder.BrowseName = new QualifiedName(path, NamespaceIndex);
+            folder.DisplayName = new LocalizedText("en", name);
+            folder.WriteMask = AttributeWriteMask.None;
+            folder.UserWriteMask = AttributeWriteMask.None;
+            folder.EventNotifier = EventNotifiers.None;
+
+            if (parent != null)
+            {
+                parent.AddChild(folder);
+            }
+
+            return folder;
+        }
+
+        protected virtual BaseDataVariableState CreateVariable(NodeState? parent, string? path, string name, BuiltInType dataType, int valueRank)
+        {
+            return CreateVariable(parent, path, name, (uint)dataType, valueRank);
+        }
+
+        protected virtual BaseDataVariableState CreateVariable(NodeState? parent, string? path, string name, NodeId dataType, int valueRank)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                path = parent?.NodeId.Identifier is string id ? id + "_" + name : name;
+
+            BaseDataVariableState variable = new BaseDataVariableState(parent);
+            variable.SymbolicName = name;
+            variable.ReferenceTypeId = ReferenceTypes.Organizes;
+            variable.TypeDefinitionId = VariableTypeIds.BaseDataVariableType;
+            variable.NodeId = new NodeId(path, NamespaceIndex);
+            variable.BrowseName = new QualifiedName(path, NamespaceIndex);
+            variable.DisplayName = new LocalizedText("en", name);
+            variable.WriteMask = AttributeWriteMask.None;
+            variable.UserWriteMask = AttributeWriteMask.None;
+            variable.DataType = dataType;
+            variable.ValueRank = valueRank;
+            variable.AccessLevel = AccessLevels.CurrentReadOrWrite;
+            variable.UserAccessLevel = AccessLevels.CurrentReadOrWrite;
+            variable.Historizing = false;
+            variable.Value = Opc.Ua.TypeInfo.GetDefaultValue(dataType, valueRank, Server.TypeTree);
+            variable.StatusCode = StatusCodes.Good;
+            variable.Timestamp = DateTime.UtcNow;
+
+            if (valueRank == ValueRanks.OneDimension)
+            {
+                variable.ArrayDimensions = new ReadOnlyList<uint>(new List<uint> { 0 });
+            }
+            else if (valueRank == ValueRanks.TwoDimensions)
+            {
+                variable.ArrayDimensions = new ReadOnlyList<uint>(new List<uint> { 0, 0 });
+            }
+
+            if (parent != null)
+            {
+                parent.AddChild(variable);
+            }
+
+            return variable;
+        }
+
+        public void UpdateValue(NodeId nodeId, object value)
+        {
+            var variable = (BaseDataVariableState)FindPredefinedNode(nodeId, typeof(BaseDataVariableState));
+            if (variable != null)
+            {
+                variable.Value = value;
+                variable.Timestamp = DateTime.UtcNow;
+                variable.ClearChangeMasks(SystemContext, false);
+            }
+        }
+
+        public NodeId AddFolder(NodeId parentId, string? path, string name)
+        {
+            var node = Find(parentId);
+            var newNode = CreateFolder(node, path, name);
+            AddPredefinedNode(SystemContext, node);
+            return newNode.NodeId;
+        }
+
+        public NodeId AddVariable(NodeId parentId, string? path, string name, BuiltInType dataType, int valueRank)
+        {
+            return AddVariable(parentId, path, name, (uint)dataType, valueRank);
+        }
+
+        public NodeId AddVariable(NodeId parentId, string? path, string name, NodeId dataType, int valueRank)
+        {
+            var node = Find(parentId);
+            var newNode = CreateVariable(node, path, name, dataType, valueRank);
+            AddPredefinedNode(SystemContext, node);
+            return newNode.NodeId;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Opc.Ua;
+using Opc.Ua.Configuration;
+using Opc.Ua.Server;
+
+
+
+namespace SampleOpcUaServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 启动OPC UA服务器
+            ApplicationInstance application = new ApplicationInstance();
+            application.ConfigSectionName = "OpcUaServer";
+            application.LoadApplicationConfiguration(false).Wait();
+            application.CheckApplicationInstanceCertificate(false, 0).Wait();
+
+            var server = new StandardServer();
+            var nodeManagerFactory = new NodeManagerFactory();
+            server.AddNodeManager(nodeManagerFactory);
+            application.Start(server).Wait();
+
+            // 模拟数据
+            var nodeManager = nodeManagerFactory.NodeManager;
+            var simulationTimer = new System.Timers.Timer(1000);
+            var random = new Random();
+            simulationTimer.Elapsed += (sender, EventArgs) =>
+            {
+                nodeManager?.UpdateValue("ns=2;s=Root_Test", 110);
+            };
+            simulationTimer.Start();
+
+            // 输出OPC UA Endpoint
+            Console.WriteLine("Endpoints:");
+            foreach (var endpoint in server.GetEndpoints().GroupBy(x => x.EndpointUrl).Select(g => g.First()))
+            {
+                Console.WriteLine(endpoint.EndpointUrl);
+            }
+
+            Console.WriteLine("按Enter添加新变量");
+            Console.ReadLine();
+
+            // 添加新变量
+            nodeManager?.AddVariable("ns=2;s=Root", null, "Test2", (int)BuiltInType.Int16, ValueRanks.Scalar);
+            Console.WriteLine("已添加变量");
+            Console.ReadLine();
+        }
+    }
+}
